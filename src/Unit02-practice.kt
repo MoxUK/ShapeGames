928e1e73863003758fc1074f1791ec952d4894aa
ShapeGames Automation Course <soerendiversen@gmail.com>
2025-08-06 17:55:52 +0200

    Assignment 8 added and completed. Leftover comments removed

--- src/Unit02-practice.kt
+++ src/Unit02-practice.kt
@@ -105,31 +105,4 @@ fun main()  {
     messageCount++
     println(messageCount)
 
-    /*
-    println("Hello world")
-    println("Hello Kotlin")
-
-    val name: String = "Artyom"
-
-    var age: Int = 30
-    age = 31
-
-    val a = 10
-    val b = 5
-    val c = a + b
-    println(c)
-
-    val isGreater : Boolean = a > b
-    println(isGreater)
-
-    var count = 1
-    println(count)
-    count++
-    print(count)
-
-    print("Enter a name: ")
-    val yourName = readLine()
-    println("Your name $yourName")
-
-     */
 }
