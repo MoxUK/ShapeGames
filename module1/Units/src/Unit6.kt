84e0d2e84cdfc54a3f6400f07520cd9f3ae074e9
ShapeGames Automation Course <soerendiversen@gmail.com>
2025-08-13 13:25:51 +0200

    Branch Merged to main

--- module1/Units/src/Unit6.kt
+++ module1/Units/src/Unit6.kt
@@ -9,7 +9,10 @@
 fun main()  {
     unit6task1()
     unit6task2()
-
+    unit6task3()
+    unit6task4()
+    unit6task5()
+    unit6task6()
 }
 fun unit6task1()    {
     println("\nFor loop - i in range from 1 to 9")
@@ -50,4 +53,54 @@ fun unit6task2()    {
         }
         println()
     }
+}
+
+fun unit6task3()    {
+    println("\nArrays")
+    val fruits =arrayOf("apple", "banana", "orange")
+    for (fruit in fruits){
+        println(fruit)
+    }
+}
+
+// 'while' loop
+fun unit6task4()    {
+    println("\nWhile loop")
+    var i = 10
+    while (i >0)    {
+        println("While loop: i=$i")
+        i--
+    }
+}
+
+// 'do while' loop
+fun unit6task5()    {
+    println("\n'Do While' loop")
+    var i = -1
+    do {
+        println("i = $i")
+        i--
+    } while (i > 0)
+}
+
+// 'continue' and 'break' in loops
+fun unit6task6()    {
+    println("\n'continue' and 'break' in loops")
+    //continue - skips the action for the matching condition and continue with the rest
+    println("\n'continue'")
+    for (i in 1 .. 9)   {
+        if (i == 5) {
+            continue
+        }
+        println("i=$i")
+    }
+    //break stops loop when condition is met
+    println("\n'break'")
+    for (i in 1..9){
+        if (i == 5) {
+            break
+        }
+        println("i=$i")
+    }
+
 }
