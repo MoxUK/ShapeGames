15eeafa292295aa60f532e3961a17f90a9c867e9
ShapeGames Automation Course <soerendiversen@gmail.com>
2025-09-01 10:44:03 +0200

    Kotlin Exercise from W3R: 'Sum of Digits in the String' 'Even Number' 'Average Value of List' 'Filter list of strings starting with "K"'

--- module1/W3RExercises/src/FilterListAndShowK-entries.kt
+++ module1/W3RExercises/src/FilterListAndShowK-entries.kt
@@ -2,5 +2,12 @@
 // It returns only strings starting with the letter 'K'.
 
 fun main()  {
+    val listOfWords= listOf("Aardwark", "Icecream", "Box", "Laptop", "Cup", "Kotlin", "Koolaid",
+        "Charger", "keychain", "Desk fan", "Monitor", "knife", "Mouse pad")
+
+
+    val filteredList = listOfWords.filter { it.startsWith("k") || it.startsWith("K") }
+    println(filteredList)
+
 
 }
