c8e645564a9e4e3f75175a9fc6452e0d1c28e0cd
ShapeGames Automation Course <soerendiversen@gmail.com>
2025-09-02 14:30:26 +0200

    Chapter 3 unit 1 to ??? added

---
+++ module1/Chapter3/src/entity/Person.kt
@@ -0,0 +1,13 @@
+package entity
+
+class Person(_name: String, _age: Int) {
+    var name: String
+    var age: Int = 18
+
+    init {
+        name = _name
+        if ((_age > 0) && (_age < 100)) {
+            age = _age
+        }
+    }
+}
