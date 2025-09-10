2e86ad95ccbee46e98aff6c200ce5a91e15c75a3
ShapeGames Automation Course <soerendiversen@gmail.com>
2025-09-10 10:33:42 +0200

    Project Exercise - MovieDB Specification added

---
+++ module1/MovieDBProject/src/projectSpecification.kt
@@ -0,0 +1,18 @@
+/**
+Create a Movie Database
+
+Project should include Media as a class with Movie and TVSeries as subclasses:
+- Movie class should include Title, Release Year, Runtime, Synopsis, Genre, Director, Actors, User Rating
+- TVSeries class should include Title, Pilot Episode Premier date, Synopsis, Genre, Actors, User Rating
+Additionally, sub-categorise with Season/Episode overview
+- User class including Username, Favorites (list)
+
+
+Functionality:
+Search for Movie or TV series by name
+Search for Actor
+- list all movies actor has acted in.
+Add/Remove Movie/TV series to user's favourite list
+Update user rating of movie/TV series
+
+ */
